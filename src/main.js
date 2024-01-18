e86b0c72b3c463f23b024d0cc02dfbe2a64829fe
Hyukk2 <alsgurdl258@kakao.com>
2024-01-18 16:29:05 +0900

    JS-main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 const headerArea = document.querySelector(".header");
 const headerRect = headerArea.getBoundingClientRect();
+const headerNav = document.querySelector('.header__nav');
+const headerMenu = document.querySelector('.header__menu');
+const headerToggle = document.querySelector('.header__toggle');
 
-const homeArea = document.querySelector("#home");
-const homeInDivArea = document.querySelector(".home__opacity");
-const homeRect = homeArea.getBoundingClientRect();
+const homeArea = document.querySelector(".home__container");
 
-// 아래로 스크롤시, header 범위에 background-color 적용
+const arrowUpArea = document.querySelector('.arrow-up');
+
 window.addEventListener("scroll", () => {
   headerChangeBg();
   homeChangeOpacity();
+  showArrowUp();
 });
 
+// 브라우저 너비 768px이하 일 때, 보이는 btn 클릭시
+headerToggle.addEventListener('click', showHeaderMenu);
+
+// menu list 클릭시
+headerNav.addEventListener('click', blindHeaderMenu);
+
+// header background 변경
 function headerChangeBg() {
   if (window.scrollY > headerRect.height) {
     headerArea.classList.add("header--dark");
@@ -19,4 +29,26 @@ function headerChangeBg() {
   }
 }
 
-function homeChangeOpacity() {}
+// home opacity 변경
+function homeChangeOpacity() {
+  let opacity = 1 - window.scrollY / homeArea.offsetHeight;
+  opacity = opacity < 0 ? 0 : opacity;
+  homeArea.style.opacity = opacity.toString();
+}
+
+// arrowUp 보여주기
+function showArrowUp() {
+  arrowUpArea.style.opacity = window.scrollY / homeArea.offsetHeight > 0.5 ? '1' : '0';
+  arrowUpArea.style.visibility = window.scrollY / homeArea.offsetHeight > 0.5 ? 'visible' : 'hidden';
+}
+
+function showHeaderMenu() {
+  headerMenu.classList.toggle('open');
+}
+
+function blindHeaderMenu(event) {
+  const target = event.target;
+  if(target.classList.contains('header__menu__item')) {
+    headerMenu.classList.remove('open');
+  }
+}
