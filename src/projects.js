f11311dfa983e70c236b962002b3f4bf1728f630
Hyukk2 <alsgurdl258@kakao.com>
2024-01-20 16:24:58 +0900

    JS-projects

--- src/projects.js
+++ src/projects.js
@@ -2,14 +2,20 @@
 
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
 
-categories.addEventListener('click', showFilter);
-
-function showFilter(event) {
+categories.addEventListener('click', (event) => {
   const filter = event.target.dataset.category;
   if(!filter) {
     return;
   }
+  selectedMenuReset(event.target);
+  showAnimation();
+  showFilter(filter);
+});
+
+// 프로젝트 필터링
+function showFilter(filter) {
   projects.forEach(project => {
     if(filter === 'all' || filter === project.dataset.type) {
       project.style.display = 'block';
@@ -17,4 +23,19 @@ function showFilter(event) {
       project.style.display = 'none';
     }
   });
+}
+
+// selected 메뉴 재설정
+function selectedMenuReset(target) {
+  const selected = document.querySelector('.category--selected');
+  selected.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+// 프로젝트 범위 애니메이션
+function showAnimation() {
+  projectsContainer.classList.add('animation');
+  setTimeout(() => {
+    projectsContainer.classList.remove('animation');
+  },300);
 }
