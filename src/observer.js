9822ce04b7fc9ad0523104b43a7ddfadac7d0159
Hyukk2 <alsgurdl258@kakao.com>
2024-01-21 16:22:13 +0900

    JS-IntersectionObserver

--- src/observer.js
+++ src/observer.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const headerMenuItems = document.querySelectorAll('.header__menu__item');
 let observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
+    const headerMenuItem = Array.from(headerMenuItems).find(item => item.dataset.section === entry.target.id);
     if(entry.isIntersecting) {
-      entry.target.style.backgroundColor = 'red';
+      headerMenuItem.classList.add('selected');
+    } 
+    else {
+      headerMenuItem.classList.remove('selected');
     }
-  })
-}, { threshold: 0.7 });
+  });
+}, { threshold: 0.6 });
 
 const sectionAreas = document.querySelectorAll('.main>section');
 sectionAreas.forEach((sectionTag) => {
+  console.log(sectionTag.dataset);
   observer.observe(sectionTag);
 });
 
